01df222cf97940d7fb6421d25859185d10384dbd
cakra <cakrapradiptakuntara2@gmail.com>
2024-09-25 11:17:04 +0700

    update durasi

--- components/Education.vue
+++ components/Education.vue
@@ -3,12 +3,12 @@
       <h2>Education</h2>
       <ul class="list-group">
         <li class="list-group-item">
-          <strong>Universitas Brawijaya</strong> (2023 - Now)
-          <p>Prodi Teknologi Informasi Fakultas Vokasi Universitas Brawijaya</p>
-        </li>
-        <li class="list-group-item">
           <strong>SMKN 1 JAKARTA</strong> (2019 - 2023)
           <p>Jurusan Sistem Informatika Jaringan dan Aplikasi</p>
+        </li>
+        <li class="list-group-item">
+          <strong>Universitas Brawijaya</strong> (2023 - Now)
+          <p>Prodi Teknologi Informasi Fakultas Vokasi Universitas Brawijaya</p>
         </li>
       </ul>
     </section>
--- components/Experience.vue
+++ components/Experience.vue
@@ -5,6 +5,7 @@
             <div class="card-body">
                 <h5 class="card-title">{{ exp.company }}</h5>
                 <span class="badge badge-primary">{{ exp.role }}</span>
+                <p class="text-muted">{{ exp.duration }}</p>
                 <ul>
                     <li v-for="(job, jobIndex) in exp.jobdesk" :key="jobIndex">{{ job }}</li>
                 </ul>
@@ -21,6 +22,7 @@ export default {
                 {
                     company: 'PSP FPIK UB',
                     role: 'Website Administrator',
+                    duration: 'Juli 2021 - Saar ini',
                     jobdesk: [
                         'Mengkonfigurasi plugin website agar sesuai',
                         'Mengkonfigurasi tema agar website memiliki tampilan yang baik',
@@ -30,6 +32,7 @@ export default {
                 {
                     company: 'PUSMENDIK',
                     role: 'Technical Team',
+                    duration: 'Juli 2022 - Desember 2022',
                     jobdesk: [
                         'Menjalankan query ke database untuk mengambil data',
                         'Mengkonfigurasi promotheus dan Grafana untuk memonitoring server',
@@ -42,6 +45,7 @@ export default {
                 {
                     company: 'WISECON',
                     role: 'DCT Support',
+                    duration: 'Januari 2023 - April 2023',
                     jobdesk: [
                         'Melakukan support kepada user yang terkendala melalui Helpdesk',
                         'Menjalankan query ke database untuk mengambil data',
